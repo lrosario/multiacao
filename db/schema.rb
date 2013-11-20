3b57d618a0f79224a4e6693b6031dbb14c822972
Leonardo Rosário <leonardo@localhost.localdomain>
2013-11-20 14:28:00 -0300

    Usuario Gerente

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121105181657) do
+ActiveRecord::Schema.define(:version => 20131120151913) do
 
   create_table "clientes", :force => true do |t|
     t.string   "codigo"
