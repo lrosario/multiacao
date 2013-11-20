c65d7f2497dd5faacb2d97372e635d8a97075923
Leonardo Rosário <leonardo@localhost.localdomain>
2013-11-20 14:27:48 -0300

    Usuario Gerente

--- db/migrate/20131120151913_add_user_gerente.rb
+++ db/migrate/20131120151913_add_user_gerente.rb
@@ -1,7 +1,7 @@
 class AddUserGerente < ActiveRecord::Migration
   def up
   end
-Usuario.create(:id => '1', :nome => 'Gerente', :senha => '123', :email => 'gerente@multiacao.com.br')
+Usuario.create(:id => '1', :login => 'Gerente', :senha => '123', :email => 'gerente@multiacao.com.br')
   def down
   end
 end
